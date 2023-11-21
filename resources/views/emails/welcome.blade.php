924be128845d47fd5304f4598b75ca625ff734ea
kamrul1275 <kamrul1275.info@gmail.com>
2023-11-22 00:23:35 +0600

    first commit

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+<body>
+    <p>Hello {{ $user->name }},</p>
+    <p>Welcome to our application! Thank you for registering.</p>
+    <!-- Additional content or styling can be added here -->
+</body>
+</html>
